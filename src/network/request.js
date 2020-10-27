f05c3cae9db0dc04cc73cf9621215249d345a6dd
monody517 <71575782+monody517@users.noreply.github.com>
2020-10-27 15:50:51 +0800

    Update request.js

--- src/network/request.js
+++ src/network/request.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 export function request(config) {
   const instance = axios.create({
-    baseURL: 'http://152.136.185.210:8000/api/w6',
+    baseURL: '',
     timeout: 150000
   })
 
